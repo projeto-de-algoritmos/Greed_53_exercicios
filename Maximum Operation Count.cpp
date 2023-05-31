1deed6d475cd19cb5212cf5becba7ab0f58c6a9a
DaviSilva25 <daviunb25@gmail.com>
2023-05-30 22:51:07 -0300

    Add Comments

--- Maximum Operation Count.cpp
+++ Maximum Operation Count.cpp
@@ -3,36 +3,54 @@
 #include <algorithm>
 
 using namespace std;
+
 int main() {
-	int N, K;
+    int N, K;
 
-	cin >> N >> K;
-	
-	vector<int> vetor(N);
+    // Lê os valores de N e K fornecidos pelo usuário
+    cin >> N >> K;
 
-	for(int i=0; i < N; i++)
-		cin >> vetor[i];
-	
-	vector<bool> visitado(N, false);
+    // Cria um vetor chamado 'vetor' com tamanho N para armazenar os valores
+    vector<int> vetor(N);
 
-	int comeco = 0, finale = N/2, aux = 0;
+    // Lê N valores do usuário e atribui aos elementos do vetor 'vetor'
+    for (int i = 0; i < N; i++)
+        cin >> vetor[i];
 
-	sort(vetor.begin(), vetor.end());
+    // Cria um vetor chamado 'visitado' com tamanho N e inicializa todos os elementos como falso
+    vector<bool> visitado(N, false);
 
-	while(finale < N){
-		if(vetor[finale] - vetor[comeco] >= K){
-			aux++;
-			visitado[comeco] = true;
-			visitado[finale] = true;
-			finale++;
+    // Variáveis para controlar os índices de início, fim e contador auxiliar
+    int comeco = 0, finale = N / 2, aux = 0;
 
-			while(visitado[comeco])
-				comeco++;
-		}
-		else
-			finale++;
-	}
-	cout << aux << endl;
+    // Ordena o vetor em ordem crescente
+    sort(vetor.begin(), vetor.end());
 
-	return 0;
+    // Loop principal que percorre os elementos do vetor
+    while (finale < N) {
+        // Verifica se a diferença entre os elementos atuais é maior ou igual a K
+        if (vetor[finale] - vetor[comeco] >= K) {
+            // Incrementa o contador auxiliar
+            aux++;
+
+            // Marca os elementos como visitados
+            visitado[comeco] = true;
+            visitado[finale] = true;
+
+            // Avança para o próximo elemento
+            finale++;
+
+            // Encontra o próximo índice de início não visitado
+            while (visitado[comeco])
+                comeco++;
+        } else {
+            // Incrementa o índice de fim
+            finale++;
+        }
+    }
+
+    // Imprime o valor do contador auxiliar
+    cout << aux << endl;
+
+    return 0;
 }
--- Operation with X.cpp
+++ Operation with X.cpp
@@ -10,20 +10,30 @@ int main() {
     cin >> n >> x;
  
     vector<int> vetor(n);
-    
+ 
+    // Lê os valores do vetor a partir da entrada padrão
     for(int i=0; i < n; i++) 
         cin >> vetor[i];
-        
-    //ordena o vetor
+
+    // Ordena o vetor em ordem crescente
     sort(vetor.begin(), vetor.end());
  
+    // Inicializa a diferença mínima com o maior intervalo possível inicialmente
     diferencaMinima = vetor[n-1] - vetor[0];
  
+    // Itera sobre os elementos do vetor (exceto o primeiro)
     for (int i = 1; i < n; i++) {
+        // Calcula o valor de aumento como o máximo entre o valor anterior somado a x e o último valor subtraído por x
         aumento = max(vetor[i-1] + x, vetor[n-1] - x);
+        
+        // Calcula o valor de redução como o mínimo entre o primeiro valor somado a x e o valor atual subtraído por x
         reducao = min(vetor[0] + x, vetor[i] - x);
+        
+        // Calcula a diferença entre aumento e redução e compara com a diferença mínima atual
+        // Atualiza a diferença mínima se a nova diferença for menor
         diferencaMinima = min(diferencaMinima, aumento - reducao);
     }
  
-    cout<< diferencaMinima << endl;
+    // Imprime a diferença mínima encontrada
+    cout << diferencaMinima;
 }
